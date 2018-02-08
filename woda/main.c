82da51edfe1c4018dbd7e33614c0c9d19d5dea9a
Marcin Brzeziński <32923504+marcinbrzezinski@users.noreply.github.com>
2018-02-08 12:55:44 +0100

    Slupki

---
+++ woda/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "woda.h"
+
+int main() {
+    int histogram[N] = {1, 2, 1, 1, 4, 2, 1, 3, 1};
+
+    printf("Maksymalny stan zgromadzonej wody wynosi %d.\n", policzWode(histogram));
+
+    return 0;
+}
